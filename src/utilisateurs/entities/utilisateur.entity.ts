8e86c9323de8975d98b4ad3bb52f026420624de1
Jdavidson <jd9743101@gmail.com>
2023-05-26 18:50:46 +0400

    reset

--- src/utilisateurs/entities/utilisateur.entity.ts
+++ src/utilisateurs/entities/utilisateur.entity.ts
@@ -10,7 +10,7 @@ export class Utilisateur {
     username: string
     @Column()
     email: string
-    @Column("long raw", { nullable: true })
+    @Column({ nullable: true })
     picture: string
     @OneToMany(() => Repa, (repas) => repas.user)
     repas: Repa[]
